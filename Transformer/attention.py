bdba09d0fec09b24efa69fbe7fa536866dbfaa47
Ruhaan18 <raiyandalal8@gmail.com>
2024-09-07 13:41:17 +0530

    modified the attention

--- Transformer/attention.py
+++ Transformer/attention.py
@@ -4,14 +4,14 @@ from torch.nn import functional as F
 import math
 
 class SelfAttention(nn.Module):
-    def __init__(self, n_heads, d_embed, in_proj_bias=True, out_proj_bias=True):
+    def __init__(self, n_heads, d_model, in_proj_bias=True, out_proj_bias=True):
         super().__init__()
 
-        self.in_proj = nn.Linear(d_embed, 3 * d_embed, bias=in_proj_bias)
+        self.in_proj = nn.Linear(d_model, 3 * d_model, bias=in_proj_bias)
 
-        self.out_proj = nn.Linear(d_embed, d_embed, bias=out_proj_bias)
+        self.out_proj = nn.Linear(d_model, d_model, bias=out_proj_bias)
         self.n_heads = n_heads
-        self.d_head = d_embed // n_heads
+        self.d_head = d_model // n_heads
 
     def forward(self, x, causal_mask=False):
 
